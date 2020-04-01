3b90d41c221f763126be6d3997c58fac2ce412a2
Jashema Panapa <jcpanapa@gmail.com>
2020-04-01 02:52:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   new_collection = []
 i = 0 
 while i < collection.length 
-  new_collection << yield(collection[i])
+  new_collection.upcase << yield(collection[i])
   i = i + 1 
   end 
 new_collection
